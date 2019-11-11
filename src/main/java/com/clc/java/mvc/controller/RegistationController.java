b5f1efcfb81809536419e16362b9e723123ec63d
KrutikaBobade <krutikabobade31.gmail.com>
2019-11-11 17:51:11 +0530

    controller name

--- src/main/java/com/clc/java/mvc/controller/RegistationController.java
+++ src/main/java/com/clc/java/mvc/controller/RegistationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import com.clc.java.mvc.service.impl.RegistationServiceImpl;
 
-@Controller("controller")
+@Controller("controller1")
 public class RegistationController {
 	@Autowired
 	public RegistationServiceImpl regisService;
